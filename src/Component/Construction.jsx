3c25d127b73dd2251544104b57d149095e260c5c
adityakumar-blip <adityakamal303@gmail.com>
2022-06-01 14:28:12 +0530

    construction 1.0

--- src/Component/Construction.jsx
+++ src/Component/Construction.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Construction = () => {
   return (
     <section>
-        <div className="main-container" style={{backgroundColor:'red'}}>
-            <h3 style={{textAlign:'center'}} >We're Under Construction</h3>
+        <div className="main-container" style={{backgroundColor:'black', padding:'0.2rem', display:'flex', justifyContent:'center', alignItems:'center'}}>
+            <h5 style={{textAlign:'center', color:'white'}} >We're Under Construction!</h5>
         </div>
     </section>
   )
